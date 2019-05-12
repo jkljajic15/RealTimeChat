2939738ff11769d35ccdf9ff7e8aa2dba423bd7a
Jovan Kljajic <jovankljajic93@gmail.com>
2019-05-12 16:44:35 +0200

    Add project files.

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public class Korisnici
+        {
+            public string KonekcijaId { get; set; }
+            public string UserName { get; set; }
+            public string LoginTime { get; set; }
+        }
+
+        static List<Korisnici> KonektovaniKorisnici = new List<Korisnici>();
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+            if (KonektovaniKorisnici.Count(x => x.KonekcijaId == id) == 0)
+            {
+                string logintime = DateTime.Now.ToString();
+                KonektovaniKorisnici.Add(new Korisnici { KonekcijaId = id, UserName = userName, LoginTime = logintime });
+
+                Clients.Caller.onConnected(id, userName, KonektovaniKorisnici, logintime);
+
+                Clients.AllExcept(id).onNewUserConnected(id, userName, logintime);
+            }
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            var item = KonektovaniKorisnici.FirstOrDefault(x => x.KonekcijaId == Context.ConnectionId);
+            if (item != null)
+            {
+                KonektovaniKorisnici.Remove(item);
+
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.UserName);
+
+            }
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ Chat/Scripts/js.js
@@ -0,0 +1,76 @@
+﻿$(function () {
+
+    var chatHub = $.connection.chatHub;
+
+    registerClientMethods(chatHub);
+    $.connection.hub.start().done(function () {
+        $.connection.hub.logging = true;
+        //console.log("test");
+        registerEvents(chatHub);
+
+    });
+
+});
+
+function registerEvents(chatHub) {
+
+    let name = $("#username").attr('data-value');
+    //console.log(name);
+
+    if (name != null) {
+        if (name.length > 0) {
+            chatHub.server.connect(name);
+        }
+    }
+
+
+}
+
+function registerClientMethods(chatHub) {
+
+    chatHub.client.onConnected = function (id, userName, sviKorisnici, vreme) {
+
+        //console.log(userName, sviKorisnici, vreme);
+
+        for (i = 0; i < sviKorisnici.length; i++) {
+            //console.log(sviKorisnici[i].UserName);
+            AddUser(sviKorisnici[i].KonekcijaId, sviKorisnici[i].UserName, sviKorisnici[i].LoginTime);
+        }
+    }
+
+    // On New User Connected
+    chatHub.client.onNewUserConnected = function (id, name, loginDate) {
+        //console.log("onNewUserConnected", name, loginDate);
+        AddUser(id, name, loginDate);
+    }
+
+    // On User Disconnected
+    chatHub.client.onUserDisconnected = function (id, userName) {
+
+        $('#Div' + id).remove();
+
+        //var ctrId = 'private_' + id;
+        //$('#' + ctrId).remove();
+
+
+        var disc = $('<div class="disconnect">"' + userName + '" logged off.</div>');
+
+        $(disc).hide();
+        $('#divusers').prepend(disc);
+        $(disc).fadeIn(200).delay(1000).fadeOut(200);
+
+    }
+}
+
+function AddUser(id, name, date) {
+
+    //console.log("AddUser");
+
+    code = $('<div class="box-comment" id="Div' + id + '">' +
+        ' <div class="comment-text">' +
+        '<span class="username">' + '<a id="' + name + '" class="user" >' + name + '<a>' +
+        '<span class="text-muted pull-right">' + date + '</span>  </span></div></div>');
+
+    $("#divusers").append(code);
+
+}
